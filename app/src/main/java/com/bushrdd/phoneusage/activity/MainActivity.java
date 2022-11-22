7a68c96cefc2f019fb54333295707032754f1d91
Bushrdd <836878498@qq.com>
2022-11-22 14:14:34 +0800

    [增加]文字转语音

--- app/src/main/java/com/bushrdd/phoneusage/activity/MainActivity.java
+++ app/src/main/java/com/bushrdd/phoneusage/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.bushrdd.phoneusage.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -14,11 +16,14 @@ import com.bushrdd.phoneusage.view.VideosListViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
+    private final String TAG = "MainActivity";
 
     private RecyclerView mListView;
     private VideosListViewAdapter mAdapter;
+    private TextToSpeech mTextToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +43,50 @@ public class MainActivity extends Activity {
             list.add(new VideoData(i, "哈哈哈"));
         }
         mAdapter = new VideosListViewAdapter(list);
+        mAdapter.setRecyclerviewItemListener(mListener);
         mListView.setAdapter(mAdapter);
+
+        mTextToSpeech = new TextToSpeech(this, mOnInitListener);
     }
 
+
+    private final VideosListViewAdapter.IRecyclerviewItemListener mListener = new VideosListViewAdapter.IRecyclerviewItemListener() {
+        @Override
+        public void click(int position) {
+
+        }
+
+        @Override
+        public void read(String text) {
+            mTextToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    };
+
+    private TextToSpeech.OnInitListener mOnInitListener = new TextToSpeech.OnInitListener() {
+        @Override
+        public void onInit(int status) {
+            if (status == TextToSpeech.SUCCESS) {
+                //设置首选语言为中文,注意,语言可能是不可用的,结果将指示此
+                int result = mTextToSpeech.setLanguage(Locale.CHINA);
+                if (result == TextToSpeech.LANG_MISSING_DATA ||
+                        result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    //语言数据丢失或不支持该语言。
+                    Log.e(TAG, "语言数据丢失或不支持该语言!");
+                }
+            } else {
+                // 初始化失败
+                Log.e(TAG, "朗读功能初始化失败!");
+            }
+        }
+    };
+
     public void opSetting(View view) {
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mTextToSpeech.stop(); // 不管是否正在朗读TTS都被打断
+        mTextToSpeech.shutdown(); // 关闭,释放资源
     }
 }
--- app/src/main/java/com/bushrdd/phoneusage/view/VideosListViewAdapter.java
+++ app/src/main/java/com/bushrdd/phoneusage/view/VideosListViewAdapter.java
@@ -3,6 +3,7 @@ package com.bushrdd.phoneusage.view;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,7 @@ public class VideosListViewAdapter extends RecyclerView.Adapter<VideosListViewAd
     private final String TAG = "VideosListViewAdapter";
 
     private List<VideoData> mData;
+    private IRecyclerviewItemListener mRecyclerviewItemListener;
 
     public VideosListViewAdapter(List<VideoData> data) {
         mData = data;
@@ -36,6 +38,14 @@ public class VideosListViewAdapter extends RecyclerView.Adapter<VideosListViewAd
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.textView.setText(mData.get(position).getName());
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mRecyclerviewItemListener!=null){
+                    mRecyclerviewItemListener.read((String) holder.textView.getText());
+                }
+            }
+        });
     }
 
     @Override
@@ -44,11 +54,22 @@ public class VideosListViewAdapter extends RecyclerView.Adapter<VideosListViewAd
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout layout;
         TextView textView;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            layout = itemView.findViewById(R.id.video_list_item_name_view);
             textView = itemView.findViewById(R.id.video_list_item_name);
         }
+    }
+
+    public interface IRecyclerviewItemListener {
+        void click(int position);
+        void read(String text);
+    }
+
+    public void setRecyclerviewItemListener(IRecyclerviewItemListener recyclerviewItemListener) {
+        mRecyclerviewItemListener = recyclerviewItemListener;
     }
 }
